1a2a0173194989d9fcb8303fcf3017d82b7161c7
Jeffrey Lazar <jlazar@holygpu7c1311.rc.fas.harvard.edu>
2022-06-13 11:25:44 -0400

    Move new proposal loss to its own function

--- hebe/lepton_prop.py
+++ hebe/lepton_prop.py
@@ -85,6 +85,91 @@ def _old_proposal_losses(
     total_loss = None
     return losses, total_loss
 
+def _new_proposal_losses(
+    prop,
+    p_def, 
+    energy, 
+    soft_losses,
+    direction=(0, 0, -1), 
+    position=(0,0,0),
+    propagation_length=1e5
+):
+    losses = {}
+    losses['continuous'] = []
+    losses['epair'] = []
+    losses['brems'] = []
+    losses['ioniz'] = []
+    losses['photo'] = []
+    # TODO move this to units file or some shit
+    GeV_to_MeV = 1e3
+    MeV_to_GeV = 1e-3
+    m_to_cm = 1e2
+    cm_to_m = 1/m_to_cm
+    position = position * m_to_cm
+    propagation_length = propagation_length * m_to_cm
+    # TODO: Check if init state needs to be set
+    init_state = pp.particle.ParticleState()
+    init_state.position = pp.Cartesian3D(
+        *position
+    )
+    init_state.energy = energy * GeV_to_MeV
+    init_state.direction = pp.Cartesian3D(*direction)
+    secondarys = prop.propagate(init_state, propagation_length)  # cm
+
+    for loss in secondarys.stochastic_losses():
+        # dist = loss.position.z / 100
+        e_loss = loss.energy / 1e3
+
+        """
+        dir = np.asarray([loss.direction.x, loss.direction.y, loss.direction.z])
+        
+        p = position + dist * direction
+        t = dist / Constants.c_vac + time
+        """
+        # dir = np.asarray([loss.direction.x, loss.direction.y, loss.direction.z])
+        log_sec_energy = np.log10(loss.energy * MeV_to_GeV)
+        pos = np.array([loss.position.x, loss.position.y, loss.position.z]) * cm_to_m
+        # TODO make this more compact with dict
+        loss_type_name = pp.particle.Interaction_Type(loss.type)
+        if loss_type_name == pp.particle.Interaction_Type.epair:
+            losses['epair'].append([log_sec_energy, pos])
+        elif loss_type_name == pp.particle.Interaction_Type.brems:
+            losses['brems'].append([log_sec_energy, pos])
+        elif loss_type_name == pp.particle.Interaction_Type.ioniz:
+            losses['ioniz'].append([log_sec_energy, pos])
+        elif loss_type_name == pp.particle.Interaction_Type.photonuclear:
+            losses['photo'].append([log_sec_energy, pos])
+        else:
+            pass
+
+    # TODO: Update this ugly fix
+    cont_loss_sum = sum([np.log10(loss.energy * MeV_to_GeV) for loss in secondarys.continuous_losses()])
+    total_dist = secondarys.track_propagated_distances()[-1] * cm_to_m
+    # TODO: Add this to config
+    cont_resolution = 1.
+    loss_dists = np.arange(0, total_dist, cont_resolution)
+    # TODO: Remove this really ugly fix
+    if len (loss_dists) == 0:
+        cont_loss_sum = 1.
+        total_dist = 1.1
+        loss_dists = np.array([0., 1.])
+    e_loss = cont_loss_sum / len(loss_dists)
+    # TODO: Check direction and position
+    losses['continuous'] = ([
+        [e_loss,
+         dist * direction + position]
+        for dist in loss_dists])
+    # TODO This should probably be an awkward array
+    losses['continuous'] = np.array(losses['continuous'], dtype='object')
+    losses['brems'] = np.array(losses['brems'], dtype='object')
+    losses['epair'] = np.array(losses['epair'], dtype='object')
+    losses['photo'] = np.array(losses['photo'], dtype='object')
+    losses['ioniz'] = np.array(losses['ioniz'], dtype='object')
+    if soft_losses:
+        total_loss = np.sum([len(losses[loss]) for loss in losses])
+    else:
+        total_loss = np.sum([len(losses[loss]) for loss in losses if loss != 'continuous'])
+    return losses, total_loss
 
 def jeff_losses(
     prop,
@@ -116,105 +201,15 @@ def jeff_losses(
             propagation_length=propagation_length
         )
     else:
-        losses = {}
-        losses['continuous'] = []
-        losses['epair'] = []
-        losses['brems'] = []
-        losses['ioniz'] = []
-        losses['photo'] = []
-        # TODO move this to units file or some shit
-        GeV_to_MeV = 1e3
-        MeV_to_GeV = 1e-3
-        m_to_cm = 1e2
-        cm_to_m = 1/m_to_cm
-        position = position * m_to_cm
-        propagation_length = propagation_length * m_to_cm
-        # particle = pp.particle.DynamicData(p_def.particle_type)
-        # particle.position = pp.Vector3D(*position)
-        # particle.direction = pp.Vector3D(*direction)
-        # particle.propagated_distance = 0
-        # particle.energy = energy * GeV_to_MeV
-        # secondarys = prop.propagate(particle, propagation_length)
-        # TODO: Check if init state needs to be set
-        init_state = pp.particle.ParticleState()
-        init_state.position = pp.Cartesian3D(
-            *position
+        return _new_proposal_losses(
+            prop,
+            p_def,
+            energy,
+            soft_losses,
+            direction=direction,
+            position=position,
+            propagation_length=propagation_length
         )
-        init_state.energy = energy * GeV_to_MeV
-        init_state.direction = pp.Cartesian3D(*direction)
-        secondarys = prop.propagate(init_state, propagation_length)  # cm
-
-        # for sec in secondarys.particles:
-        #     log_sec_energy = np.log10((sec.parent_particle_energy - sec.energy) * MeV_to_GeV)
-        #     pos = np.array([sec.position.x, sec.position.y, sec.position.z])*cm_to_m
-        #     # TODO make this more compact with dict
-        #     if sec.type == int(pp.particle.Interaction_Type.ContinuousEnergyLoss):
-        #         losses['continuous'].append([log_sec_energy, pos])
-        #     elif sec.type == int(pp.particle.Interaction_Type.Epair):
-        #         losses['epair'].append([log_sec_energy, pos])
-        #     elif sec.type == int(pp.particle.Interaction_Type.Brems):
-        #         losses['brems'].append([log_sec_energy, pos])
-        #     elif sec.type == int(pp.particle.Interaction_Type.DeltaE):
-        #         losses['ioniz'].append([log_sec_energy, pos])
-        #     elif sec.type == int(pp.particle.Interaction_Type.NuclInt):
-        #         losses['photo'].append([log_sec_energy, pos])
-        #     else:
-        #         pass
-        # New interface
-        for loss in secondarys.stochastic_losses():
-            # dist = loss.position.z / 100
-            e_loss = loss.energy / 1e3
-
-            """
-            dir = np.asarray([loss.direction.x, loss.direction.y, loss.direction.z])
-            
-            p = position + dist * direction
-            t = dist / Constants.c_vac + time
-            """
-            # dir = np.asarray([loss.direction.x, loss.direction.y, loss.direction.z])
-            log_sec_energy = np.log10(loss.energy * MeV_to_GeV)
-            pos = np.array([loss.position.x, loss.position.y, loss.position.z]) * cm_to_m
-            # TODO make this more compact with dict
-            loss_type_name = pp.particle.Interaction_Type(loss.type)
-            if loss_type_name == pp.particle.Interaction_Type.epair:
-                losses['epair'].append([log_sec_energy, pos])
-            elif loss_type_name == pp.particle.Interaction_Type.brems:
-                losses['brems'].append([log_sec_energy, pos])
-            elif loss_type_name == pp.particle.Interaction_Type.ioniz:
-                losses['ioniz'].append([log_sec_energy, pos])
-            elif loss_type_name == pp.particle.Interaction_Type.photonuclear:
-                losses['photo'].append([log_sec_energy, pos])
-            else:
-                pass
-
-        # TODO: Update this ugly fix
-        cont_loss_sum = sum([np.log10(loss.energy * MeV_to_GeV) for loss in secondarys.continuous_losses()])
-        total_dist = secondarys.track_propagated_distances()[-1] * cm_to_m
-        # TODO: Add this to config
-        cont_resolution = 1.
-        loss_dists = np.arange(0, total_dist, cont_resolution)
-        # TODO: Remove this really ugly fix
-        if len (loss_dists) == 0:
-            cont_loss_sum = 1.
-            total_dist = 1.1
-            loss_dists = np.array([0., 1.])
-        e_loss = cont_loss_sum / len(loss_dists)
-        # TODO: Check direction and position
-        losses['continuous'] = ([
-            [e_loss,
-             dist * direction + position]
-            for dist in loss_dists])
-        # TODO This should probably be an awkward array
-        losses['continuous'] = np.array(losses['continuous'], dtype='object')
-        losses['brems'] = np.array(losses['brems'], dtype='object')
-        losses['epair'] = np.array(losses['epair'], dtype='object')
-        losses['photo'] = np.array(losses['photo'], dtype='object')
-        losses['ioniz'] = np.array(losses['ioniz'], dtype='object')
-        if soft_losses:
-            total_loss = np.sum([len(losses[loss]) for loss in losses])
-        else:
-            total_loss = np.sum([len(losses[loss]) for loss in losses if loss != 'continuous'])
-        return losses, total_loss
 
 def _make_particle(p_string: str):
     ''' Builds a proposal particle
