f4b5222c40b20c6f014209cc9f48eae1a5a00617
Jeffrey Lazar <jlazar@holygpu7c1311.rc.fas.harvard.edu>
2022-06-22 16:04:19 -0400

    .

--- examples/fasrc_example_ppc.py
+++ examples/fasrc_example_ppc.py
@@ -11,25 +11,26 @@ import os
 jconfig.update("jax_enable_x64", True)
 
 def main(args=None):
-    if args is None:
-        args = 1337
+    if len(args)==1:
+        rset = 1337
         print("Using default seed!")
     else:
         rset = int(args[1])
     print('CURRENT SET %d' % rset)
-    pname = 'MuMinus'
+    pname = 'TauMinus'
     config["general"]["random state seed"] = rset
     config["general"]["meta_name"] = 'meta_data_%d' % rset
     scratch_dir = "/n/holyscratch01/arguelles_delgado_lab/Everyone/jlazar/muons/data/"
     config['photon propagator']['storage location'] = f'{scratch_dir}/rset_{rset}_'
-    nevent = 5
+    nevent = 1
     config['lepton injector']['location'] = "/n/holylfs05/LABS/arguelles_delgado_lab/Lab/common_software/lib64/"
     config['lepton injector']['xsec location'] = "/n/holylfs05/LABS/arguelles_delgado_lab/Lab/common_software/source/LeptonInjector/resources/"
     config['lepton injector']['simulation']['output name'] = "./data_%d_output_LI.h5" % rset
     config['lepton injector']['simulation']['nevents'] = nevent
-    config['lepton injector']['simulation']['minimal energy'] = 1e4
-    config['lepton injector']['simulation']['maximal energy'] = 1e5
-    config['lepton injector']['simulation']['is ranged'] = True
+    config['lepton injector']['simulation']['final state 1'] = pname
+    config['lepton injector']['simulation']['minimal energy'] = 1e7
+    config['lepton injector']['simulation']['maximal energy'] = 1e7
+    config['lepton injector']['simulation']['is ranged'] = False
     config['lepton injector']['simulation']["injection radius"] = 800
     config['lepton injector']['simulation']["endcap length"] = 800
     config['lepton injector']['simulation']["cylinder radius"] = 700
@@ -39,18 +40,18 @@ def main(args=None):
     config["detector"]["file name"] = '../hebe/data/icecube-f2k'
     photo_prop = "PPC_CUDA"
     config['photon propagator']['name'] = photo_prop
-    config['photon propagator'][photo_prop]['ppc_tmpfile'] = f"/n/home12/jlazar/hebe_new/examples/.{rset}_event_hits.ppc.tmp"
-    config['photon propagator'][photo_prop]['f2k_tmpfile'] = f"/n/home12/jlazar/hebe_new/examples/.{rset}_event_losses.f2k.tmp"
+    config['photon propagator'][photo_prop]['ppc_tmpfile'] = f"/n/home12/jlazar/hebe/examples/.{rset}_event_hits.ppc.tmp"
+    config['photon propagator'][photo_prop]['f2k_tmpfile'] = f"/n/home12/jlazar/hebe/examples/.{rset}_event_losses.f2k.tmp"
     config['photon propagator'][photo_prop]['location'] = "/n/holylfs05/LABS/arguelles_delgado_lab/Lab/common_software/source/PPC_CUDA/"
     config['photon propagator'][photo_prop]['ppctables'] = "/n/holylfs05/LABS/arguelles_delgado_lab/Lab/common_software/source/PPC_CUDA/"
     config['photon propagator'][photo_prop]['ppc_exe'] = "/n/holylfs05/LABS/arguelles_delgado_lab/Lab/common_software/source/PPC_CUDA/ppc"
-    config['run']['group name'] = 'RangedInjector0'
+    config['run']['group name'] = 'VolumeInjector0'
     hebe = HEBE(userconfig=config)
     hebe.sim()
     for idx in range(nevent):
         print('Plotting')
         event = {'final_1':[hebe.results['final_1'][idx]], 'final_2':[hebe.results['final_2'][idx]]}
-        hebe.ppc_event_plotting(event, fig_name=f'./{pname}_test_{idx}.pdf', show_track=False, show_dust_layer=True)
+        hebe.ppc_event_plotting(event, fig_name=f'./{pname}_{rset}_{idx}.pdf', show_track=False, show_dust_layer=True)
 
     #hebe.sim()
     del hebe
