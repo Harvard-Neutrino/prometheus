e9ddaf629dc04cf32970fa59807bec07dde365dc
Jeffrey Lazar <jlazar@holygpu7c1311.rc.fas.harvard.edu>
2022-07-01 16:51:35 -0400

    Add detector to init

--- hebe/__init__.py
+++ hebe/__init__.py
@@ -5,5 +5,6 @@ from .config import config
 from .utils import __init__
 from .particle import Particle
 from .lepton_prop import __init__
+from .detector import Detector, detector_from_f2k
 
 __all__ = (HEBE, config)
