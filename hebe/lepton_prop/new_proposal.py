984008ee968d4aa3429622a5294f9f267a7896c5
Jeffrey Lazar <jlazar@holygpu7c1311.rc.fas.harvard.edu>
2022-07-26 14:13:27 -0400

    premerge commit

--- hebe/lepton_prop/new_proposal.py
+++ hebe/lepton_prop/new_proposal.py
@@ -12,47 +12,183 @@ from ..particle import Particle
 from ..utils import iter_or_rep
 from  ..utils.units import GeV_to_MeV, cm_to_m, m_to_cm, MeV_to_GeV
 
+def make_pdef(pstr):
+    '''
+    Builds a proposal particle definition
+
+    Parameters
+    ----------
+    pstr: string
+        String which defines the medium in which proposal should propagate
+
+    Returns
+    -------
+    pdef : proposal.particle.ParticleDef
+        PROPOSAL particle definition object corresponing to pstr
+    '''
+    if pstr in 'MuMinus MuPlus EMinus EPlus TauMinus TauPlus'.split():
+        pdef = getattr(pp.particle, f'{pstr}Def')()
+    else:
+        raise ValueError(
+            f"Particle string {pstr} not recognized"
+        )
+    return pdef
+
+def make_propagator(
+    pstr,
+    **kwargs
+):
+    # Make the PROPOSAL utilities object
+    ecut = kwargs["ecut"]
+    vcut = kwargs["vcut"]
+    if hasattr(vcut, "__iter__") and len(ecut)!=2:
+        raise ValueError("You can only specify vcut lists of length 2")
+    if hasattr(ecut, "__iter__") and len(ecut)!=2:
+        raise ValueError("You can only specify ecut lists of length 2")
+
+    pdef = make_pdef(pstr)
+    medium = _make_medium(kwargs["medium_str"])
+    # TODO figure out how to get the rest of the options working
+    do_continuous_randomization = kwargs["continuous_randomization"]
+    # List have been passed for both energy cut settings
+    if hasattr(vcut, "__iter__") and hasattr(ecut, "__iter__"):
+        utilities = [
+            make_utility(
+                pdef,
+                interpolate,
+                pp.EnergyCutSettings(ec, vc, do_continuous_randomization),
+                medium
+            )
+        for ec, vc in zip(ecut, vcut)]
+    # A List was passed for vcut only
+    elif hasattr(vcut, "__iter"):
+        utilities = [
+            make_utility(
+                pdef,
+                interpolate,
+                pp.EnergyCutSettings(ecut, vc, do_continuous_randomization),
+                medium
+            )
+        for vc in vcut]
+    # A List was passed for ecut only
+    elif hasattr(ecut, "__iter"):
+        utilities = [
+            make_utility(
+                pdef,
+                interpolate,
+                pp.EnergyCutSettings(ec, vcut, do_continuous_randomization),
+                medium
+            )
+        for ec in ecut]
+    # Scalars were passed for both energy cut settings
+    else:
+        utilities = [
+            make_utility(
+                pdef,
+                interpolate,
+                pp.EnergyCutSettings(ecut, vcut, do_continuous_randomization),
+                medium
+            )
+        for _ in range(2)]
+
+    # Make the PROPOSAL geometries
+    inner_r = [0, kwargs["r_detector"]]
+    outer_r = [kwargs["r_detector"], kwargs["r_max"]]
+    geometries = [
+        pp.geometry.Sphere(
+            pp.Cartesian3D(),
+            end * m_to_cm,
+            start * m_to_cm
+        )
+        for start, end in zip(outer_r, inner_r)
+    ]
+
+    # Make the density distributions
+    density_distrs = [
+        pp.density_distribution.density_homogeneous(
+            medium.mass_density
+        ) for _ in range(2)
+    ]
+
+    prop = pp.Propagator(
+        particle_def,
+        list(zip(geometries, utilities, density_distrs))
+    )
+
+    return prop
+
+def make_utility(
+    particle_def,
+    interpolate,
+    cuts,
+    medium
+):
+    collection = pp.PropagationUtilityCollection()
+    cross = pp.crosssection.make_std_crosssection(
+        particle_def,
+        medium,
+        cuts,
+        interpolate
+    )
+    # TODO look into continuous randomization
+    create_tables = True
+    collection.displacement = pp.make_displacement(cross, create_tables)
+    collection.interaction = pp.make_interaction(cross, create_tables)
+    collection.time = pp.make_time(cross, particle_def, create_tables)
+    collection.decay = pp.make_decay(cross, particle_def, create_tables)
+
+    utility = pp.PropagationUtility(collection=collection)
+
+    return utility
+
+def _make_medium(medium_string):
+    '''
+    Builds a proposal medium
+
+    Parameters
+    ----------
+    medium_string: string
+        String which defines the medium in which proposal should propagate
+
+    Returns
+    -------
+    medium_def : PROPOSAL medium object
+        Medium in which the propagation should take place
+    '''
+    print('This assumes a homogeneous medium!')
+    if medium_string.lower() not in 'water ice'.split():
+        raise ValueError(f"Medium {medium_string} not supported at this time.")
+    else:
+        medium_def = getattr(pp.medium, medium_string.capitalize())()
+    return medium_def
+
+def _init_pp_particle(particle, pdef):
+    # Function must be called for each new particle.
+    # Maybe. Or we might just be able to overwrite the
+    # information but that requires some care. Makes me a bit nervous
+    init_state = pp.particle.ParticleState()
+    init_state.position = pp.Cartesian3D(
+        *particle.position
+    )
+    init_state.energy = particle.energy * GeV_to_MeV
+    init_state.direction = pp.Cartesian3D(*particle.direction)
+    return init_state
+
 def _new_proposal_losses(
     prop,
     p_def, 
-    energy, 
-    soft_losses,
-    direction=(0, 0, -1), 
-    position=(0,0,0),
-    propagation_length=1e5
+    particle,
+    padding,
+    r_inice
 ):
-    losses = {}
-    losses['continuous'] = []
-    losses['epair'] = []
-    losses['brems'] = []
-    losses['ioniz'] = []
-    losses['photo'] = []
-    position = position * m_to_cm
-    propagation_length = propagation_length * m_to_cm
     # TODO: Check if init state needs to be set
-    init_state = pp.particle.ParticleState()
-    init_state.position = pp.Cartesian3D(
-        *position
-    )
-    init_state.energy = energy * GeV_to_MeV
-    init_state.direction = pp.Cartesian3D(*direction)
-    secondarys = prop.propagate(init_state, propagation_length)  # cm
+    init_state = _init_pp_particle(particle, pdef)
+    propagation_length = np.linalg.norm(particle.position) + padding
+    secondarys = prop.propagate(init_state, propagation_length * m_to_cm)
 
     for loss in secondarys.stochastic_losses():
-        # dist = loss.position.z / 100
-        e_loss = loss.energy / 1e3
-
-        """
-        dir = np.asarray([loss.direction.x, loss.direction.y, loss.direction.z])
-        
-        p = position + dist * direction
-        t = dist / Constants.c_vac + time
-        """
-        # dir = np.asarray([loss.direction.x, loss.direction.y, loss.direction.z])
-        log_sec_energy = np.log10(loss.energy * MeV_to_GeV)
+        loss_energy = loss.energy * MeV_to_GeV
         pos = np.array([loss.position.x, loss.position.y, loss.position.z]) * cm_to_m
-        # TODO make this more compact with dict
-        loss_type_name = pp.particle.Interaction_Type(loss.type)
         if loss_type_name == pp.particle.Interaction_Type.epair:
             losses['epair'].append([log_sec_energy, pos])
         elif loss_type_name == pp.particle.Interaction_Type.brems:
@@ -65,21 +201,20 @@ def _new_proposal_losses(
             pass
 
     # TODO: Update this ugly fix
-    cont_loss_sum = sum([np.log10(loss.energy * MeV_to_GeV) for loss in secondarys.continuous_losses()])
+    cont_loss_sum = np.sum(secondarys.continuous_losses()) * MeV_to_GeV
     total_dist = secondarys.track_propagated_distances()[-1] * cm_to_m
     # TODO: Add this to config
     cont_resolution = 1.
     loss_dists = np.arange(0, total_dist, cont_resolution)
     # TODO: Remove this really ugly fix
     if len (loss_dists) == 0:
-        cont_loss_sum = 1.
+        cont_loss_sum = 1. * GeV_to_MeV
         total_dist = 1.1
         loss_dists = np.array([0., 1.])
     e_loss = cont_loss_sum / len(loss_dists)
-    # TODO: Check direction and position
     losses['continuous'] = ([
         [e_loss,
-         dist * direction + position]
+         dist * particle.direction + particle.position]
         for dist in loss_dists])
     # TODO This should probably be an awkward array
     losses['continuous'] = np.array(losses['continuous'], dtype='object')
