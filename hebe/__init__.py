f55ee8f9258cec9dc1c1a350e14fea7997fbaabf
Jeffrey Lazar <jlazar@holylogin04.rc.fas.harvard.edu>
2022-12-16 14:53:44 -0500

    Add import from injection

--- hebe/__init__.py
+++ hebe/__init__.py
@@ -5,6 +5,7 @@ from .config import config
 from .utils import __init__
 from .particle import Particle
 from .lepton_prop import __init__
+from .injection import __init__
 from .detector import Detector, detector_from_f2k
 
 __all__ = (HEBE, config)
