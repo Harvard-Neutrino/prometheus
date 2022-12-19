985e432e6e9ccb6501d62d393464afb9564f7b5b
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2022-12-19 10:43:19 -0500

    Move utility functions to utils

---
+++ hebe/utils/serialize_particles_to_awkward.py
@@ -0,0 +1,23 @@
+import awkward as ak
+
+def serialize_particles_to_awkward(
+    det,
+    particles,
+):
+
+    # Only create array if any particles made light
+    if not any([len(p.hits) > 0 for p in particles]):
+        return None
+
+    hit_functions = [
+        ("string_id", lambda particles: [[h[0] for h in p.hits] for p in particles]),
+        ("sensor_id", lambda particles: [[h[1] for h in p.hits] for p in particles]),
+        ("t", lambda particles: [[h[2] for h in p.hits] for p in particles]),
+        ("sensor_pos_x", lambda particles: [[det[(h[0], h[1])].pos[0] for h in p.hits] for p in particles]),
+        ("sensor_pos_y", lambda particles: [[det[(h[0], h[1])].pos[1] for h in p.hits] for p in particles]),
+        ("sensor_pos_z", lambda particles: [[det[(h[0], h[1])].pos[2] for h in p.hits] for p in particles]),
+    ]
+
+    outarr = ak.Array({k:f(particles) for k, f in hit_functions})
+
+    return outarr
