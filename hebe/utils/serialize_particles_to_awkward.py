6395bddd12d2b284023b2fc9baa230fe534cde8a
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2022-12-20 16:07:52 -0500

    Make this not slow again

--- hebe/utils/serialize_particles_to_awkward.py
+++ hebe/utils/serialize_particles_to_awkward.py
@@ -1,4 +1,5 @@
 import awkward as ak
+import numpy as np
 
 def serialize_particles_to_awkward(
     det,
@@ -9,15 +10,25 @@ def serialize_particles_to_awkward(
     if not any([len(p.hits) > 0 for p in particles]):
         return None
 
+    xyz = [
+        np.array([det[(h.string_id, h.om_id)].pos for h in p.hits]) for p in particles
+    ]
+    outdict = {}
+    for idx, var in enumerate("x y z".split()):
+        outdict[f"sensor_pos_{var}"] = [x[:, idx] for x in xyz]
+
     hit_functions = [
-        ("string_id", lambda particles: [[h[0] for h in p.hits] for p in particles]),
-        ("sensor_id", lambda particles: [[h[1] for h in p.hits] for p in particles]),
-        ("t", lambda particles: [[h[2] for h in p.hits] for p in particles]),
-        ("sensor_pos_x", lambda particles: [[det[(h[0], h[1])].pos[0] for h in p.hits] for p in particles]),
-        ("sensor_pos_y", lambda particles: [[det[(h[0], h[1])].pos[1] for h in p.hits] for p in particles]),
-        ("sensor_pos_z", lambda particles: [[det[(h[0], h[1])].pos[2] for h in p.hits] for p in particles]),
+        ("string_id", lambda particles: [[h.string_id for h in p.hits] for p in particles]),
+        ("sensor_id", lambda particles: [[h.om_id for h in p.hits] for p in particles]),
+        ("t", lambda particles: [[h.time for h in p.hits] for p in particles]),
+        #("sensor_pos_x", lambda particles: [[det[(h.string_id, h.om_id)].pos[0] for h in p.hits] for p in particles]),
+        #("sensor_pos_y", lambda particles: [[det[(h.string_id, h.om_id)].pos[1] for h in p.hits] for p in particles]),
+        #("sensor_pos_z", lambda particles: [[det[(h.string_id, h.om_id)].pos[2] for h in p.hits] for p in particles]),
     ]
 
-    outarr = ak.Array({k:f(particles) for k, f in hit_functions})
+    for field, fxn in hit_functions:
+        outdict[field] = fxn(particles)
+    outarr = ak.Array(outdict)
+    #outarr = ak.Array({k:f(particles) for k, f in hit_functions})
 
     return outarr
