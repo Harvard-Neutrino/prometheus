1f63c09782505aedb2b90bc831006391dde6b7d6
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2022-12-30 00:08:27 -0500

    Refactor lepton prp into subclasses

--- hebe/lepton_propagation/__init__.py
+++ hebe/lepton_propagation/__init__.py
@@ -1,2 +1,9 @@
 from .loss import Loss
-from .lepton_prop import LP
+from .lepton_propagator import LeptonPropagator
+from .old_proposal_lepton_propagator import OldProposalLeptonPropagator
+from .new_proposal_lepton_propagator import NewProposalLeptonPropagator
+
+lp_dict = {
+    "old proposal": OldProposalLeptonPropagator,
+    "new proposal": NewProposalLeptonPropagator
+}
