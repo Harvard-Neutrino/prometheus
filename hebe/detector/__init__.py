43186cb297834f91f965ef393f5185bf4d9fbcbf
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2022-12-30 00:11:03 -0500

    Make directory for detector

---
+++ hebe/detector/__init__.py
@@ -0,0 +1,11 @@
+from .module import Module
+from .detector import Detector
+from .detector_factory import (
+    detector_from_f2k,
+    detector_from_geo,
+    make_line,
+    make_grid,
+    make_hex_grid,
+    make_triang,
+    make_rhombus
+)
