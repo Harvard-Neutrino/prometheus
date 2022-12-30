1b1b2c4e63c6e3e38170dae792316217f5cb9493
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2022-12-30 11:51:16 -0500

    ALL CAPS lp_dict

--- hebe/lepton_propagation/__init__.py
+++ hebe/lepton_propagation/__init__.py
@@ -3,7 +3,7 @@ from .lepton_propagator import LeptonPropagator
 from .old_proposal_lepton_propagator import OldProposalLeptonPropagator
 from .new_proposal_lepton_propagator import NewProposalLeptonPropagator
 
-lp_dict = {
+LP_DICT = {
     "old proposal": OldProposalLeptonPropagator,
     "new proposal": NewProposalLeptonPropagator
 }
