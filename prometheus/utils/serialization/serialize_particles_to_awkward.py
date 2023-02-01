da4f946ef269fed7befc0520988929b4ffc62742
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2023-02-01 09:07:49 -0500

    id_str -> id_idx

--- prometheus/utils/serialization/serialize_particles_to_awkward.py
+++ prometheus/utils/serialization/serialize_particles_to_awkward.py
@@ -31,7 +31,7 @@ def serialize_particles_to_awkward(
         ("string_id", lambda x: [[h.string_id for h, _ in event_hits] for event_hits in x]),
         ("sensor_id", lambda x: [[h.om_id for h, _ in event_hits] for event_hits in x]),
         ("t", lambda x: [[h.time for h, _ in event_hits] for event_hits in x]),
-        ("id_str", lambda x: [[id_str for _, id_str in event_hits] for event_hits in x]),
+        ("id_idx", lambda x: [[id_str for _, id_str in event_hits] for event_hits in x]),
     ]
 
     for field, fxn in hit_functions:
