bf3908a13b9f5929179953311fbb1b66444d01aa
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2023-02-07 01:45:03 -0500

    Add docstring

--- prometheus/photon_propagation/registered_propagators.py
+++ prometheus/photon_propagation/registered_propagators.py
@@ -1,6 +1,7 @@
 from ..utils import ExtendedEnum
 
 class RegisteredPropagators(ExtendedEnum):
+    """Enum to keep track of which photon propagators we know about"""
     OLYMPUS=1
     PPC=2
     PPCCUDA=3
