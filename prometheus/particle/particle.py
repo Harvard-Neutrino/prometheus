7813b002e8c7816a8edcfde6827bfd0be703e4ca
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2023-02-07 01:37:46 -0500

    update docstrings

--- prometheus/particle/particle.py
+++ prometheus/particle/particle.py
@@ -13,16 +13,14 @@ from ..utils.translators import PDG_to_pstring
 class Particle:
     """Base dataclass for particle event structure
 
-    params
+    fields
     ______
     pdg_code: PDG mc code
     e: energy in GeV
-    position:
-
-
-    returns
-    _______
-
+    position: particle position in meters
+    direction: unit vector pointing along particle momentum
+    serialization_idx: Index helper for serialization. This
+        will be overwritten at serialization time
     """
     pdg_code: int
     e: float
@@ -47,7 +45,15 @@ class Particle:
 
 @dataclass
 class PropagatableParticle(Particle):
-    """Particle event structure with added feature to support propagation"""
+    """Particle event structure with added feature to support propagation
+    
+    fields
+    ______
+    parent: Particle which created this particle
+    children: Particles that this one spawned
+    losses: energy losses created by this particle
+    hits: OM hits originating from this particle
+    """
     parent: Particle
     children: List[Particle] = field(default_factory=list)
     losses: List = field(default_factory=list)
