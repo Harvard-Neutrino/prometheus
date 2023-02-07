19c6275bb8a543edd2e9330ae7c1da8563ac59de
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2023-02-07 00:00:47 -0500

    Make Loss a dataclass

--- prometheus/lepton_propagation/loss.py
+++ prometheus/lepton_propagation/loss.py
@@ -1,35 +1,20 @@
 import numpy as np
-from proposal import Vector3D
+from dataclasses import dataclass
 
 from ..utils import int_type_to_str
 
-class Loss(object):
-
-    def __init__(self, int_type, e, position):
-        self._int_type = int_type
-        self._e = e
-        if isinstance(position, Vector3D):
-            self._position = np.array(
-                [position.x * cm_to_m, position.y * cm_to_m, position.z * cm_to_m
-            ])
-        else:
-            self._position = np.array(position)
-        self._str = int_type_to_str[int_type]
+@dataclass(frozen=True)
+class Loss:
+    """Dataclass for handling PROPOSAL energy losses
+    params
+    ______
+    int_type: interaction type
+    e: energy lost
+    position: position of the loss in meters
+    """
+    int_type: int
+    e: float
+    position: np.ndarray
 
     def __str__(self):
-        return self._str
-
-    def __repr__(self):
-        return f"Interaction type: {self.int_type}\nEnergy: {self.e}\nPosition: {self.position}"
-
-    @property
-    def int_type(self):
-        return self._int_type
-
-    @property
-    def e(self):
-        return self._e
-
-    @property
-    def position(self):
-        return self._position
+        return int_type_to_str[self.int_type]
