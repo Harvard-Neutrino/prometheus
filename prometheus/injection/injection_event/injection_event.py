331753708ddc777741a2a8daedf80dddc9468690
Jeffrey Lazar <jlazar@holygpu8a29104.rc.fas.harvard.edu>
2023-02-06 23:29:32 -0500

    Add docstrings

--- prometheus/injection/injection_event/injection_event.py
+++ prometheus/injection/injection_event/injection_event.py
@@ -4,8 +4,19 @@ from typing import Iterable
 from .. interactions import Interactions
 from ..import Particle
 
+# TODO Why is vertex split up ? DTaSD
 @dataclass
 class InjectionEvent:
+    """Dataclass for handing injection events
+    
+    params
+    ______
+    initial_state: Incident neutrino
+    final_states: All particles which results from the interaction
+    vertex_x: x-position of the interaction vertex
+    vertex_y: x-position of the interaction vertex
+    vertex_z: x-position of the interaction vertex
+    """
     initial_state: Particle
     final_states: Iterable[Particle]
     interaction: Interactions
