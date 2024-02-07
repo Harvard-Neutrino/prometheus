24efa02dd852be96d3c7e480c1956ca02c1e2b48
Jeffrey Lazar <jlazar@holygpu7c26306.rc.fas.harvard.edu>
2024-02-07 09:22:29 -0500

    Remove old examples

--- examples/run_olympus.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for (( counter=1000; counter>0; counter-- ))
-do
-python example_olympus.py $counter
-done
-printf "\n"
--- examples/run_ppc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for (( counter=1; counter>0; counter-- ))
-do
-python example_ppc.py $counter
-done
-printf "\n"
