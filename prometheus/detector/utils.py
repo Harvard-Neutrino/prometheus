0a918d12d37cbe2c5384c0097e7b576d89552914
chivoi <a.lastovirya@gmail.com>
2026-03-16 17:19:01 -0500

    normalize prometheus/detector docstrings

--- prometheus/detector/utils.py
+++ prometheus/detector/utils.py
@@ -2,20 +2,23 @@ import random
 import string
 
 def random_serial() -> str:
-    """Makes a serial number in the hexdecimal form expected by PPC
+    """Makes a serial number in the hexdecimal form expected by ppc.
 
-    returns
-    _______
-    serial: Hexadecimal OM serial number
+    Returns
+    -------
+    serial : str
+        Hexadecimal OM serial number.
     """
     serial = "0x"+"".join(random.choices('0123456789abcdef', k=12))
     return serial
 
 def random_mac() -> str:
-    """Makes a MAC ID in the form expected by PPC
+    """Creates a random MAC ID in the form expected by ppc.
     
-    returns:
-    mac: MAC IC
+    Returns
+    -------
+    mac : str
+        MAC ID.
     """
     mac = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
     return mac
